3fe7e67ad24bcf4e356ddc332a922c7dedd9d0ac
Tobber55 <118307664+Tobber55@users.noreply.github.com>
2024-12-13 12:30:50 -0500

    Add files via upload

--- src/Driver.java
+++ src/Driver.java
@@ -1,21 +1,7 @@
 public class Driver {
-
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         clear();
         System.out.println("Console cleared!");
     }
-
-    public static void clear() {
-        try {
-            if (System.getProperty("os.name").contains("Windows")) {
-                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            } else {
-                new ProcessBuilder("clear").inheritIO().start().waitFor();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
   }
-
