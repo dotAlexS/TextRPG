153fbba7a78244bcf8c72c3ea6adeb1841259147
Alex <ashao60@stuy.edu>
2024-12-14 21:36:04 -0500

    Add Dice class

---
+++ src/Adventureres/Dice.java
@@ -0,0 +1,24 @@
+public class Dice {
+    public Dice() {
+    }
+
+    public static int d20() {
+        return (int) (Math.random() * 20) + 1;
+    }
+
+    public static int d10() {
+        return (int) (Math.random() * 10) + 1;
+    }
+
+    public static int coin() {
+        return (int) (Math.random() * 2) + 1;
+    }
+
+    public static int advantage20() {
+        int a = (int) (Math.random() * 20) + 1;
+        int b = (int) (Math.random() * 20) + 1;
+        if (a > b)
+            return a;
+        return b;
+    }
+}
