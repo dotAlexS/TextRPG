d4e2ac5c337d61779bee0f6790f78b7b8a436a3a
Alex <ashao60@stuy.edu>
2024-12-14 21:05:57 -0500

    Moved Driver File

--- src/Driver.java
+++ src/Driver.java
@@ -1,7 +1,99 @@
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 public class Driver {
     public static void main(String[] args) {
 
         
 
+        MainMenu start = new MainMenu();
+        start.displayMenu();
+
+        keyDetection(null, null, start, true);
+
+        //clear();
+        //room.displayMap();
+
+        //MoveTest player = new MoveTest(2, 2);
+
+        //room.spawnEntity(2, 2, 2, "O");
+
+        //keyDetection(room, player);
+
     }
-  }
+
+    public static void startGame(MainMenu start) {
+        Map room = new Room1(20,20);
+        room.setBrackets(start.getvisibleBrackets());
+        room.displayMap();
+        Entity player = new Entity(2, 2);
+        room.spawnEntity(2, 2, 2, "O");
+        if (start.getinputDetection() == true) {
+            keyDetection(room, player, start, false);
+        }
+        else {
+            ///////////////////////////////////////////////////////////////  ADD NO KEY DETECTION CODE HERE
+        }
+    }
+
+    public static void keyDetection(Map room, Entity player, MainMenu start, boolean inMenu) {
+        JFrame frame = new JFrame("");
+        frame.setSize(20, 20);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Add KeyListener for input
+        frame.addKeyListener(new KeyListener() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                try {
+                    if (inMenu == false) {
+                        switch (e.getKeyChar()) {
+                            case 'w': if(player.canMoveCheck(1, 1, room)){   
+                                room.moveEntity(1, 1, player.getX(), player.getY()); 
+                                player.moveY(-1);
+                                break;
+                            } else break;
+                            case 'a': if(player.canMoveCheck(4, 1, room)){
+                                room.moveEntity(4, 1, player.getX(), player.getY());
+                                player.moveX(-1); 
+                                break;
+                            } else break;
+                            case 's': if ( player.canMoveCheck(3, 1, room) ){
+                                room.moveEntity(3, 1, player.getX(), player.getY()); 
+                                player.moveY(1); 
+                                break;
+                            } else break;
+                            case 'd': if (player.canMoveCheck(2, 1, room) ){      
+                                room.moveEntity(2, 1, player.getX(), player.getY()); 
+                                player.moveX(1); 
+                                break;
+                            } else break;
+                        }
+                    }
+                    else {
+                        switch (e.getKeyChar()) {
+                            case 'w': start.step(-1); start.displayMenu(); break;
+                            case 's': start.step(1); start.displayMenu(); break;
+                            case ' ': start.select(); start.displayMenu(); if(start.getOption() == 0) {frame.removeKeyListener(this); frame.dispose(); startGame(start);} break;
+                            //frame.removeKeyListener(this);
+                        }
+                    }
+                } catch (Exception ex) {
+                    room.displayMap();
+                    System.out.println();
+                    System.out.println("You cannot move out of bounds!");
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) { }
+            @Override
+            public void keyTyped(KeyEvent e) { }
+        });
+
+        frame.setVisible(true);
+    }
+    
+}
