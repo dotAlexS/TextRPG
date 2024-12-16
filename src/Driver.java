a4a8a6a1da0cc053d97167b4b812c51946d04668
Tobber55 <118307664+Tobber55@users.noreply.github.com>
2024-12-16 12:24:29 -0500

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -33,11 +33,44 @@ public class Driver {
         if (start.getinputDetection() == true) {
             keyDetection(room, player, start, false);
         } else {
-            /////////////////////////////////////////////////////////////// ADD NO KEY
-            /////////////////////////////////////////////////////////////// DETECTION CODE
-            /////////////////////////////////////////////////////////////// HERE
+          while (true) {
+            Scanner myObj = new Scanner(System.in);
+            String option = myObj.nextLine();
+            if (option.equals("w")) {
+              if (player.canMoveCheck(1, 1, room)) {
+                  room.moveEntity(1, 1, player.getX(), player.getY());
+                  player.moveY(-1);
+                  continue;
+              }
+            }
+            if (option.equals("a")) {
+              if (player.canMoveCheck(4, 1, room)) {
+                  room.moveEntity(4, 1, player.getX(), player.getY());
+                  player.moveX(-1);
+                  continue;
+              }
+            }
+            if (option.equals("s")) {
+              if (player.canMoveCheck(3, 1, room)) {
+                  room.moveEntity(3, 1, player.getX(), player.getY());
+                  player.moveY(1);
+                  continue;
+              }
+            }
+            if (option.equals("d")) {
+              if (player.canMoveCheck(2, 1, room)) {
+                  room.moveEntity(2, 1, player.getX(), player.getY());
+                  player.moveX(1);
+                  continue;
+              }
+            }
+            else {
+              System.out.println("Wrong Input. Try Again.");
+              continue;
+            }
+          }
         }
-    }
+      }
 
     public static void keyDetection(Map room, Entity player, MainMenu start, boolean inMenu) {
         JFrame frame = new JFrame("");
