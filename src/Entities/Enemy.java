967856a286d0aaffebcc481f54ee0048a3a044f8
Alex <ashao60@stuy.edu>
2024-12-17 19:25:34 -0500

    Entity Organisation

--- src/Entities/Enemy.java
+++ src/Entities/Enemy.java
@@ -2,27 +2,25 @@ package Entities;
 
 public abstract class Enemy extends Entity{
 
-    private int atkStandard;
 
-    public Enemy(String name, int maxHP, int money, int atkStandard){
-        super(name, maxHP, money);
-        this.atkStandard = atkStandard;
+    public Enemy(String name, int maxHP, int money, int atkStandard, int defense, int speed){
+        super(name, maxHP, money, atkStandard, defense, speed);
     }
 
     public int attack(){
         int roll = Dice.d20();
-        System.out.println("Enemy rolled a "+ roll);
+        System.out.println( this.getName() + " rolled a "+ roll);
         if (roll < 4){
             System.out.println(" and misses.");
             return 0;
         }
         else if (roll <= 19){
-            int dmg = (roll / 10) * atkStandard;
+            int dmg = (roll / 10) * this.getAtkStandard();
             System.out.println(" And hits for " + dmg + " damage.");
             return dmg;
         }
         else{
-            int dmg = 3 * atkStandard;
+            int dmg = 3 * this.getAtkStandard();
             System.out.println(" dealing a critical hit for " + dmg + " damage.");
             return dmg;
         }
--- src/Entities/Entity.java
+++ src/Entities/Entity.java
@@ -5,80 +5,102 @@ import Map.Map;
 public abstract class Entity {
 
     private String name;
-    private int HP, maxHP, money;
+    private int HP, maxHP, money, speed, defense, atkStandard;
+    
     public int xcor = 0;
     public int ycor = 0;
-
-    public Entity(String name, int maxHP, int money) {
+    
+    public Entity(String name, int maxHP, int money, int atkStandard, int defense, int speed) {
         this.name = name;
         this.maxHP = maxHP;
         this.money = money;
+        this.atkStandard = atkStandard;
+        this.defense = defense;
+        this.speed = speed;
     }
     public abstract int attack();
-
+    
     public void setpos(int xcor, int ycor) {
         this.xcor = xcor;
         this.ycor = ycor;
     }
-
+    
     public void moveX(int step) {
         xcor += step;
     }
-
+    
     public void moveY(int step) {
         ycor += step;
     }
-//getters and setters
+    //getters and setters
     public int getX() {
         return (xcor);
     }
-
+    
     public int getY() {
         return (ycor);
     }
-
+    
     public String getName() {
         return name;
     }
-
+    
     public void setName(String name) {
         this.name = name;
     }
-
+    public int getSpeed() {
+        return speed;
+    }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    
     public int getHP() {
         return HP;
     }
-
+    
     public void setHP(int hP) {
         HP = hP;
     }
-
+    
     public int getMaxHP() {
         return maxHP;
     }
-
+    
     public void setMaxHP(int maxHP) {
         this.maxHP = maxHP;
     }
-
+    
     public int getMoney() {
         return money;
     }
-
+    
     public void setMoney(int money) {
         this.money = money;
     }
-// end getters and setters
-
+    public int getDefense() {
+        return defense;
+    }
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+    public int getAtkStandard() {
+        return atkStandard;
+    }
+    public void setAtkStandard(int atkStandard) {
+        this.atkStandard = atkStandard;
+    }
+    // end getters and setters
+    
     public boolean canMoveCheck(int facing, int amountMoved, Map map) {
         if (facing == 1 && 0 <= ycor - amountMoved)
-            return true;
+        return true;
         else if (facing == 2 && map.sizex >= xcor + amountMoved)
-            return true;
+        return true;
         else if (facing == 3 && map.sizey >= ycor + amountMoved)
-            return true;
+        return true;
         else if (0 <= xcor - amountMoved)
-            return true;
+        return true;
         return false;
     }
 }
